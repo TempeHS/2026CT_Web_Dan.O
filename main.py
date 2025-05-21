1a608c8b3343dae06a391f7a962d78a86a342ff3
Dan O <dan.ozeri@education.nsw.gov.au>
2025-05-21 01:54:50 +0000

    card update

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ app = Flask(__name__)
 @app.route('/index.html')
 @app.route('/')
 def index():
-    return render_template("index.html"), 200
+    card_data = (
+        ("Saxophone", "saxophones", "come for saxophones", "static/images/logo.png"),
+        ("Learn Saxophone", "Want to learn how to play saxophone", "Click for topic", "static/images/logo.png"),
+        ("Saxophone", "Why a saxophone will fix your life", "I'll help you", "static/images/logo.png"),
+        ("Saxophone", "Want to basic music knowledge", "come for saxophones", "static/images/logo.png"),
+    )
+    return render_template("index.html", cards=card_data), 200
 
 
 if __name__ == '__main__':
