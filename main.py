0967c41ae2f2e1dc96322eeb14a7a929c1b4714d
Dan O <dan.ozeri@education.nsw.gov.au>
2025-05-25 23:36:12 +0000

    update cards1

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     card_data = (
-        ("Saxophone", "saxophones", "come for saxophones", "static/images/logo.png"),
-        ("Learn Saxophone", "Want to learn how to play saxophone", "Click for topic", "static/images/logo.png"),
-        ("Saxophone", "Why a saxophone will fix your life", "I'll help you", "static/images/logo.png"),
-        ("Saxophone", "Want to basic music knowledge", "come for saxophones", "static/images/logo.png"),
+        ("Saxophone", "saxophones", "come for saxophones", "static/images/Card_1.png"),
+        ("Learn Saxophone", "Want to learn how to play saxophone", "Click for topic", "static/images/Card_2.png"),
+        ("Basic skills", "Why a saxophone will fix your life", "I'll help you", "static/images/Card_3.png"),
+        ("Intruments", "Want to basic music knowledge", "come for saxophones", "static/images/Card_3.png"),
+        ("Intruments", "Want to basic music knowledge", "come for saxophones", "static/images/Card_3.png")
     )
     return render_template("index.html", cards=card_data), 200
 
 
-if __name__ == '__main__':
+if __name__ == '__main__'
     app.run(debug=True)
